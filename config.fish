16aa023a1c7fd5cde03b198949e2d80e48cba3b9
Shen-Ta Hsieh <ibmibmibm.tw@gmail.com>
2018-08-10 15:57:28 +0800

    update fish config
    Signed-off-by: Shen-Ta Hsieh <ibmibmibm.tw@gmail.com>

--- config.fish
+++ config.fish
@@ -1,25 +1,10 @@
-function sd ; sudo -i $argv; end
-function j ; jump $argv; end
-function fish_user_key_bindings
-  bind \cr peco_select_history
-end
-
-eval (tmuxifier init - fish)
-
-# Path to your oh-my-fish.
-set fish_path $HOME/.oh-my-fish
-
-# Theme
-set fish_theme robbyrussell
-
-# Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
-# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
-# Example format: set fish_plugins autojump bundler
-set -gx Z_SCRIPT_PATH /home/beststeve/.local/bin/z.sh
-set fish_plugins jump peco percol vundle tmux z
-
-# Path to your custom folder (default path is $FISH/custom)
-#set fish_custom $HOME/dotfiles/oh-my-fish
-
-# Load oh-my-fish configuration.
-source $fish_path/oh-my-fish.fish
+set -g -x POWERLINE_CONFIG /home/beststeve/.vim/bundle/repos/github.com/powerline/powerline/scripts/powerline-config
+set -g -x POWERLINE_CONFIG_COMMAND /home/beststeve/.vim/bundle/repos/github.com/powerline/powerline/scripts/powerline-config
+set -g -x POWERLINE_COMMAND /home/beststeve/.vim/bundle/repos/github.com/powerline/powerline/client/powerline
+#set -g -x VIRTUAL_ENV_DISABLE_PROMPT true
+#set fish_key_bindings fish_vi_key_bindings
+#set fish_key_bindings fish_default_key_bindings
+#function tmux
+#    /usr/bin/tmux -2 $argv
+#end
+powerline-setup
